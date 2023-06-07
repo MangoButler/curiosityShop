5fa852469241643e7846a6bcb62f7be06a0bb317
chris off <christian.hg.hegi@gmail.com>
2023-06-07 16:35:45 +0900

    added fetching of data from base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Intro from "./components/Intro/Intro";
 import MenuList from "./components/Menu/MenuList";
-import { foodItems, drinkItems } from './dummy-meals';
-// 
+// import { foodItems, drinkItems } from "./dummy-meals";
+import useRequest from "./hooks/use-request";
+//
 function App() {
+  const [foods, setFoods] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  const { isLoading, error, sendRequest } = useRequest();
+
+  useEffect(() => {
+    // const foodJson = JSON.stringify(foodItems);
+    const transformData = (setter, data) => {
+      const transformedData = [];
+      for(let key in data){
+        transformedData.push(
+          {
+            id: data[key].id,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          }
+        )
+      };
+  
+      setter(transformedData);
+    };
+
+    sendRequest(
+      {url: "https://react-http-48ff4-default-rtdb.firebaseio.com/foods.json",}, transformData.bind(null, setFoods)
+      ); 
+    sendRequest(
+        {url: "https://react-http-48ff4-default-rtdb.firebaseio.com/drinks.json",}, transformData.bind(null, setDrinks)
+      );   
+    
+
+  }, [sendRequest]);
+
   return (
     <React.Fragment>
-      <Header/>
-      <Intro/>
-      <MenuList title='Food' menuList={foodItems}/>
-      <MenuList title='Drinks' menuList={drinkItems}/>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error.message}</p>}
+      <Header />
+      <Intro />
+      <MenuList title="Food" menuList={foods} />
+      <MenuList title="Drinks" menuList={drinks} />
     </React.Fragment>
   );
 }
